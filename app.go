874394ef54587fa2fecad213b4a23ca7715e768a
sapotero <egokok@gmail.com>
2017-10-04 14:43:05 +0300

    test

--- app.go
+++ app.go
@@ -1,7 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"github.com/valyala/fasthttp"
+	"github.com/buaazp/fasthttprouter"
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+var hash = make(map[string]string)
+
+
+
+type User struct {
+	Name string `json:"Name,omitempty"`
+}
+
+type SearchResult struct {
+	Name []string `json:"results,omitempty"`
+}
+
+func Index(ctx *fasthttp.RequestCtx) {
+	log.Printf("Welcome!\n")
+}
+
+func Hello(ctx *fasthttp.RequestCtx) {
+	ctx.Response.Header.Set("X-My-Header", "my-header-value")
+
+	name := fmt.Sprintf("%s", ctx.UserValue("name"))
+
+	hash[name] = name+"_test"
+	log.Printf("Hash size : %d", len(hash))
+
+	resp, _ := json.Marshal(User{ Name: name })
+	ctx.Response.AppendBody(resp)
+}
+
+func Search(ctx *fasthttp.RequestCtx) {
+	name := fmt.Sprintf("%s", ctx.UserValue("name"))
+
+	hash[name] = name+"_test"
+	log.Printf("Hash size : %d", len(hash))
+
+	result := SearchResult{};
+
+	keys := make([]string, 0, len(hash))
+	for k := range hash {
+		keys = append(keys, k)
+	}
+
+	for _, key := range(keys) {
+		if strings.Contains(key, name) {
+			result.Name = append(result.Name, key)
+		}
+	}
+
+	resp, _ := json.Marshal(result)
+	ctx.Response.AppendBody(resp)
+
+}
 
 func main() {
-	fmt.Println("++")
+
+	router := fasthttprouter.New()
+	router.GET("/", Index)
+	router.GET("/put/:name", Hello)
+	router.GET("/search/:name", Search)
+
+	log.Fatal(fasthttp.ListenAndServe(":8080", router.Handler))
 }
