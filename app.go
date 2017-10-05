b0e0717e0d00b476e301e5a7c0eea180e296ccfe
sapotero <egokok@gmail.com>
2017-10-05 11:48:43 +0300

    add regular search

--- app.go
+++ app.go
@@ -5,52 +5,54 @@ import (
 	"github.com/valyala/fasthttp"
 	"github.com/buaazp/fasthttprouter"
 	"encoding/json"
-	"fmt"
 	"strings"
+	"flag"
+	"regexp"
+	"fmt"
 )
 
 var hash = make(map[string]string)
 
-
-
-type User struct {
-	Name string `json:"Name,omitempty"`
-}
+const (
+	defaultMaster = true
+	defaultHost   = "127.0.0.1"
+	defaultPort   = "8080"
+)
 
 type SearchResult struct {
 	Name []string `json:"results,omitempty"`
 }
 
+
 func Index(ctx *fasthttp.RequestCtx) {
 	log.Printf("Welcome!\n")
 }
 
-func Hello(ctx *fasthttp.RequestCtx) {
+func Put(ctx *fasthttp.RequestCtx) {
 	ctx.Response.Header.Set("X-My-Header", "my-header-value")
 
 	name := fmt.Sprintf("%s", ctx.UserValue("name"))
 
 	hash[name] = name+"_test"
 	log.Printf("Hash size : %d", len(hash))
-
-	resp, _ := json.Marshal(User{ Name: name })
-	ctx.Response.AppendBody(resp)
 }
 
 func Search(ctx *fasthttp.RequestCtx) {
-	name := fmt.Sprintf("%s", ctx.UserValue("name"))
+	isRegular := false;
+	name := fmt.Sprintf("%s", ctx.QueryArgs().Peek("pattern"))
 
-	hash[name] = name+"_test"
-	log.Printf("Hash size : %d", len(hash))
+	// Дикая проверка на то что прилетело ?regular=true
+	reg := fmt.Sprintf("%s", ctx.QueryArgs().Peek("regular"))
+	if len(reg) != 0 && reg == "true" {
+		isRegular = true
+		log.Print("regular true")
+	}
 
 	result := SearchResult{};
 
-	keys := make([]string, 0, len(hash))
-	for k := range hash {
-		keys = append(keys, k)
-	}
+	keys := searchByKeys(hash, name, isRegular)
 
-	for _, key := range(keys) {
+	for _, key := range keys {
 		if strings.Contains(key, name) {
 			result.Name = append(result.Name, key)
 		}
@@ -63,10 +65,49 @@ func Search(ctx *fasthttp.RequestCtx) {
 
 func main() {
 
+	flag.Bool("master", defaultMaster, "is master node")
+	flag.String("host", defaultHost, "default host")
+	flag.String("port", defaultPort, "default port")
+
+
 	router := fasthttprouter.New()
 	router.GET("/", Index)
-	router.GET("/put/:name", Hello)
-	router.GET("/search/:name", Search)
+	router.GET("/put/:name", Put)
+	router.GET("/search", Search)
 
 	log.Fatal(fasthttp.ListenAndServe(":8080", router.Handler))
+}
+
+
+
+//----------
+func searchByKeys(hash map[string]string, pattern string, isRegularExpression bool) []string {
+	log.Printf("Hash size : %d | pattern: %s | re: %t", len(hash), pattern, isRegularExpression)
+
+	keys := make([]string, 0, len(hash))
+	re := regexp.MustCompile(pattern)
+
+	if len(pattern) == 0 {
+		return keys
+	}
+
+
+	for testString := range hash {
+
+		if isRegularExpression {
+			match := re.MatchString(testString)
+			if match {
+				keys = append(keys, testString)
+			}
+		} else {
+			if strings.Contains(
+				strings.ToLower(testString),
+				strings.ToLower(pattern))  {
+				keys = append(keys, testString)
+			}
+		}
+
+
+	}
+	return keys
 }
